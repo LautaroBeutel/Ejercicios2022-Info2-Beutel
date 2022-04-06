664cc14dd4b2a8149fe04ccb08d16f9f1555776d
lautaro <lautarob52@mail.com>
2022-04-06 12:21:19 -0300

    Ejercicio 4 - no resuelto

---
+++ Ej4-Punteros/punteros.c
@@ -0,0 +1,20 @@
+/* nombre: ~Lautaro Beutel~
+ *  email: ~lautarob52@gmail.com~
+ *  fecha: ~30-03-2022~
+ */ 
+
+
+//ejercicio 4:  
+
+#include <stdio.h>
+
+
+
+
+int main(void){         //principio funcion main
+
+
+
+
+    return 0;           //finalizacion exitosa del codigo
+}//fin de de la funcion main
