8023aa015f79e0c354140ddf2d456b5b4b86f357
lautaro <lautarob52@mail.com>
2022-05-25 17:23:37 -0300

    no resuelto

---
+++ Ej10-Display7Segmentos/src/main.cpp
@@ -0,0 +1,21 @@
+//****************************************************************************//
+//  Name    : Ej10-Display7Segmentos                                          //
+//  Author  : Lautaro Beutel                                                  //
+//  Date    : 24/05/2022                                                      //
+//  Version : 1.0                                                             //
+//  Notes   : Crear un programa que desplace el led encendido de un display 7 //
+//          : segmentos para recorrer cada uno de los segmentos en orden      //
+//          : (a,b ... g, punto).                                             //
+//          : Utilizar el acceso directo a los registros de salida del        //
+//          : microcontrolador y las operaciones a nivel de bits              //
+//****************************************************************************//
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
