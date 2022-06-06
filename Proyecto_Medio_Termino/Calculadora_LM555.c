a2d82693ff9f756665f2ddf77cf0ddb985ed80d3
lautaro <lautarob52@mail.com>
2022-06-06 10:41:30 -0300

    + comentariosgit add Calculadora_LM555.c

--- Proyecto_Medio_Termino/Calculadora_LM555.c
+++ Proyecto_Medio_Termino/Calculadora_LM555.c
@@ -8,7 +8,6 @@
 //          : de los componentes seleccionados                              //
 //**************************************************************************//
 
-
 #include <stdio.h>
 
 typedef struct Componentes{ 
@@ -19,29 +18,31 @@ typedef struct Componentes{
     float Freq
 }Comp;
 
+/*Constructores de las funciones*/
 void Calculo_Frecuencia(Comp *Val);
 void Pedir_valores(Comp *Val);
+
 
 int main(void){         //principio f*uncion main
     Comp Val;
 
-    Pedir_valores(&Val);    //llamada a la funcion Pedir_valores y se le pasa como arumento la direccion de la variable tipo Comp val
+    Pedir_valores(&Val);    //llamada a la funcion Pedir_valores y se le pasa como arumento la direccion de la estructura Val de tipo Comp
     
-    Calculo_Frecuencia(&Val);   //llamada a la funcion Calculo_Frecuencia y se le pasa como argumento la direccion de la variable tipo Comp val
+    Calculo_Frecuencia(&Val);   //llamada a la funcion Calculo_Frecuencia y se le pasa como argumento la direccion de la estructura Val de tipoComp
 
     printf("\nLa frecuencia es %f Hz", Val.Freq);
-
 
     return 0;           //finalizacion exitosa del codigo
 }//fin de de la funcion main
 
 void Pedir_valores(Comp *Val){           //funcion que solicita los valores de los componentes
-    printf("%s\n", "ingrese valores de R1, R2 y C1 respectivamente(en su unidad)");
-    scanf("%f", &Val->R1);
-    scanf("%f", &Val->R2);
-    scanf("%f", &Val->C1);
-    
-    return 0;//funcion finalizada
+    printf("\n%s\n", "----------------------CALCULADORA 555-----------------------");
+    printf("%s\n", "ingrese valores de R1, R2 y C (en su unidad)");
+    printf("\tR1:");scanf("%f", &Val->R1);
+    printf("\n\tR2:");scanf("%f", &Val->R2);
+    printf("\n\tC1:");scanf("%f", &Val->C1);
+
+    return;     //funcion finalizada
 }
 
 void Calculo_Frecuencia(Comp *Val){      //funcion que realiza el calculo de la frecuencia segun el datasheet del LM555_Fairchild
@@ -49,8 +50,7 @@ void Calculo_Frecuencia(Comp *Val){      //funcion que realiza el calculo de la 
 
     float Vel_carga = (((*Val).R1 + (2 * (*Val).R2)) * (*Val).C1);
     
-
     (*Val).Freq = C / Vel_carga;
 
-    return 0;//funcion finalizada
+    return;     //funcion finalizada
 }
