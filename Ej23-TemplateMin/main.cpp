f0f2c9b8c374ec42e606541de44d4c0ac8546ac6
LautaroBeutel <lautarob52@gmail.com>
2022-09-05 01:33:44 -0300

    terminado

--- Ej23-TemplateMin/main.cpp
+++ Ej23-TemplateMin/main.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-//#define FLOTANTE
-#define ENTERO
+#define FLOTANTE
+//#define ENTERO
 
 template <class x>
 x f(x a, x b);
@@ -11,26 +11,26 @@ x f(x a, x b);
 int main(){
 #ifdef FLOTANTE
     float a = 4.5;
-    float b = 3.5;
-
-    cout << "el menor es" << f(a, b); 
+    float b = 3.5; 
 #endif
 #ifdef ENTERO
     int a = 3;
     int b = 5;
-
-    cout << "el menor es" << f(a, b);
 #endif
+    //f(a, b);
+
+    cout << "el menor es\t" << f(a, b) << endl;
     return 0; 
 }
 
 template <class x>
-x min(x a, x b){
-    if(a < b){
-        return a;
-    }
-    else
-    {
-        return b;
-    }
+x f(x a, x  b){
+	//cout << "numero_1: "<< a << "\tnumero_2: " << b << endl;
+	if(a < b){
+        	return a;
+    	}
+    	else
+    	{
+        	return b;
+    	}
 }
