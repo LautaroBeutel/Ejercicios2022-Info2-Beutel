3e2abbf0c3080ce0884198d0ddd409411aeb0497
LautaroBeutel <lautarob52@gmail.com>
2022-09-14 01:08:12 -0300

    l

--- Ej26-ClaseRectangulo/main.cpp
+++ Ej26-ClaseRectangulo/main.cpp
@@ -1,55 +1,112 @@
 #include <iostream>
 using namespace std;
 
-class Rectagulo{
-	public:
-		Rectangulo(int x, int y);
-		~Rectangulo();
-		float area();
-		void mover(int x, int y);
-		void tamaño(int base, int h);
-	private:
-		int x_, y_, base_, h_;
-		float area = 0;
+class rectangulo
+{
+private:
+	int x_, y_;
+	int b_, h_;
+public:
+	rectangulo(int x, int y, int b, int h);
+	~rectangulo();
+	int area();
+	void establecer(int x, int y, int base, int alt);
+	void leer();
+	int contiene(rectangulo a);
+	void interseccion(rectangulo a);
 };
 
-Rectangulo::Rectangulo(int x, int y, int base, int h){
+rectangulo::rectangulo(int x, int y, int b, int h)
+{
 	x_ = x;
 	y_ = y;
-	base_ = base;
+	b_ = b;
 	h_ = h;
 }
 
-float Rectangulo::area(){
-	area = base_ + h_;
+rectangulo::~rectangulo()
+{
+}
+
+int rectangulo::area(){
+	int a = (b_ * h_);
 	
-	return area;
+	return a;
 }
 
-void Rectangulo::mover(int x, int y){
+void rectangulo::establecer(int x, int y, int base, int alt){
 	x_ = x;
 	y_ = y;
+	b_ = base;
+	h_ = alt;
 }
 
-void Rectangulo::set_tamaño(int base, int h){
-	base_ = base;
-	h_ = h;
-
+void rectangulo::leer(){
+	cout << "inicio: " << "(" << x_ << "," << y_ << ")" << endl;
+	cout << "base: " << " largo: " << b_ << "alto: " << h_ << endl;
 }
 
-void Rectangulo::pos(){
-	cout << "pos:\t(" << x.x_ << "," << x.y_ << ")" << endl;
+int rectangulo::contiene(rectangulo a){
+	if(x_ < a.x_ ){
+		if (y_ > a.y_)
+		{
+			if (b_ > a.b_)
+			{
+				if (h_ > a.h_)
+				{
+					return 1;
+				}else{
+					return 0;
+				}
+			}else{
+				return 0;
+			}
+		}else{
+			return 0;
+		}
+	}else{
+		return 0;
+	}
 }
 
-void Rectangulo::leer_tamaño(){
-	cout << "tam:\t(" << x.base_ << "," << x.h_ << ")" << endl;
+void rectangulo::interseccion(rectangulo a){
+	if (contiene(a))
+	{
+		cout << "inter-area:\t" << a.area();
+	}
+	else
+	{	
+		int x = 0;
+		int y = 0;
+
+		int j = a.b_;
+		int i = a.h_;
+
+		if (b_ == a.b_)
+		{
+			int x = b_;
+		}else
+		{
+			int x = abs((b_) - (j));
+		}
+
+		if (h_ == a.h_)
+		{
+			int y = b_;
+		}else
+		{
+			int y = abs((h_) - (i));
+		}
+		
+		rectangulo r(1, 1, x, y);
+		cout << "inter-area:\t" << r.area();
+	}
 }
 
 int main(){
-	Rectangulo a = Rectangulo(1, 1, 2, 2);
-	a.pos();
-	a.leer.tamaño();
-
+	rectangulo r1(2, 2, 1, 2);
+	rectangulo r2(1, 2, 2, 2);
+	r2.interseccion(r1);
 
 	return 0;
 }
