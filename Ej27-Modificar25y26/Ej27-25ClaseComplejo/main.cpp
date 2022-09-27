82699d8f50314e303b615d1032d6436e36152299
LautaroBeutel <52940450+LautaroBeutel@users.noreply.github.com>
2022-09-26 22:57:21 -0300

    Add files via upload

---
+++ Ej27-Modificar25y26/Ej27-25ClaseComplejo/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+//template<class tikitaka>
+
+
+class complejo{
+	public:
+		complejo(int, int);
+		
+		complejo &establecer(int, int);
+		
+		void print();
+		void suma(complejo x, complejo y);
+	private:
+		int i_, j_;
+
+};
+
+complejo::complejo(int i = 0, int j = 0){
+	establecer(i, j);
+}
+
+complejo &complejo::establecer(int i, int j){
+	i_ = i;
+	j_ = j;
+	return *this;
+}
+
+void complejo::print(){
+	cout << i_ << "i\t" << j_ << "j"  << endl; 
+}
+
+void complejo::suma(complejo x, complejo y){
+	i_ = (x.i_) + (y.i_);
+	j_ = (x.j_) + (y.j_);
+	cout << "suma=\t" << i_ << "i\t" << j_ <<"j" << endl;
+}
+
+int main(){
+	complejo j(1, 3);
+	j.print();
+	return 0;
+}
+
